7cc37f357321673161e79f6086237d1d31b0b648
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-17 18:14:04 +0300

    add init of client

--- Code/main.py
+++ Code/main.py
@@ -1,7 +1,9 @@
 from gui.main_app import DMClientApp
+from DMBotNetwork import Client
 
 if __name__ == "__main__":
     app = DMClientApp()
+    Client()
     app.setup()
     app.run()
     app.cleanup()
