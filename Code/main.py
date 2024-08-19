34e3c74fc722fbcfe210b9c0ef43aef4646d380f
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-19 19:36:49 +0300

    Add loging

--- Code/main.py
+++ Code/main.py
@@ -1,9 +1,10 @@
+import logging
 from DMBotNetwork import Client
 from gui.main_app import DMClientApp
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     app = DMClientApp()
     Client()
     app.setup()
     app.run()
-    app.cleanup()
